b958d64d391ec42560ddba362a4d3153666aecc9
Ted <tchbell@gmail.com>
2020-08-10 07:33:12 -0400

    added views

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,18 +3,11 @@ const server = require("../../src/server");
 const base = "http://localhost:3000";
 
 describe("routes : static", () => {
-
-    //#1
     describe("GET /", () => {
-
-        //#2
-        it("should return status code 200", (done) => {
-
-            //#3
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-
-                //#4
+                expect(body).toContain("Welcome to Bloccit");
                 done();
             });
         });
--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const appConfig = require('./config/main-config');
-const routeConfig = require('./config/route-config.js');
+const routeConfig = require('./config/route-config');
 
-appConfig.init();
+appConfig.init(app, express);
 routeConfig.init(app);
 
 module.exports = app;
